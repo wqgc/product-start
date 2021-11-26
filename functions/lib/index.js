877672eda6afc53e693d5bf6da3d3b263a150656
wqgc <rashellehopkins@outlook.com>
2021-11-26 08:13:14 -0800

    Update eslint and gitignore

---
+++ functions/lib/index.js
@@ -0,0 +1,53 @@
+import * as functions from 'firebase-functions';
+import firebase from 'firebase-admin';
+import express from 'express';
+import Products from './models/Products.js';
+firebase.initializeApp();
+const app = express();
+const main = express();
+// Middleware
+main.use('/api/v1', app);
+// Product Routes
+// Get all products
+app.get('/products', async (_request, response) => {
+    const products = await Products.read();
+    response.json(products);
+});
+// Get one product
+app.get('/products/:id', (_request, response) => {
+    response.send('');
+});
+// Create product
+app.post('/products', (_request, response) => {
+    response.send('');
+});
+// Update product
+app.put('/products/:id', (_request, response) => {
+    response.send('');
+});
+// Delete product
+app.delete('/products/:id', (_request, response) => {
+    response.send('');
+});
+// User Routes
+// Get all users
+app.get('/users', (_request, response) => {
+    response.send('');
+});
+// Get one user
+app.get('/users/:id', (_request, response) => {
+    response.send('');
+});
+// Auth Routes
+app.post('/register', (_request, response) => {
+    response.send('');
+});
+app.get('/login', (_request, response) => {
+    response.send('');
+});
+app.get('/logout', (_request, response) => {
+    response.send('');
+});
+const api = functions.https.onRequest(main);
+export default api;
+//# sourceMappingURL=index.js.map
